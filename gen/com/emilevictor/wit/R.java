f28c6881ab5f4bb0e7f296e1cd07814836102ca4
Emile Victor <emilevictor@gmail.com>
2013-01-13 09:27:29 +1000

    Updating, up to the point where I have the numberpad.

--- gen/com/emilevictor/wit/R.java
+++ gen/com/emilevictor/wit/R.java
@@ -8,30 +8,45 @@
 package com.emilevictor.wit;
 
 public final class R {
+    public static final class array {
+        public static final int campuses=0x7f050000;
+        public static final int days=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_send=0x7f070001;
-        public static final int menu_settings=0x7f070002;
-        public static final int room_textfield=0x7f070000;
+        public static final int buildingRoomInstructionLabel=0x7f080002;
+        public static final int button_send=0x7f080006;
+        public static final int campusChoiceSpinner=0x7f080001;
+        public static final int campusLabel=0x7f080000;
+        public static final int dayLabel=0x7f080004;
+        public static final int daysSpinner=0x7f080005;
+        public static final int menu_settings=0x7f080007;
+        public static final int room_textfield=0x7f080003;
     }
     public static final class layout {
         public static final int activity_find_room_contents=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_find_room_contents=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int activity_find_room_contents=0x7f070000;
+        public static final int activity_main=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int buildingRoomInstructionLabel=0x7f040009;
         public static final int button_send=0x7f040004;
+        public static final int campusLabel=0x7f04000a;
+        public static final int dayLabel=0x7f04000b;
+        public static final int gatton_radioButton=0x7f040008;
         public static final int hello_world=0x7f040001;
+        public static final int ipswich_radioButton=0x7f040007;
         public static final int menu_settings=0x7f040002;
         public static final int room_textfield=0x7f040003;
+        public static final int stlucia_radioButton=0x7f040006;
         public static final int title_activity_find_room_contents=0x7f040005;
     }
     public static final class style {
@@ -56,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
