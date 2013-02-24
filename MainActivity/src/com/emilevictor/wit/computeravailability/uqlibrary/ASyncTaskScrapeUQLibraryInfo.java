544525e81b55e68c3a22ed2c06a3202342aa3587
Emile Victor <emilevictor@gmail.com>
2013-02-24 16:29:47 +1000

    Implementing scraping of the computer availability page.

--- MainActivity/src/com/emilevictor/wit/computeravailability/uqlibrary/ASyncTaskScrapeUQLibraryInfo.java
+++ MainActivity/src/com/emilevictor/wit/computeravailability/uqlibrary/ASyncTaskScrapeUQLibraryInfo.java
@@ -1,6 +1,7 @@
 package com.emilevictor.wit.computeravailability.uqlibrary;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -20,54 +21,17 @@ public class ASyncTaskScrapeUQLibraryInfo  extends AsyncTask<String, Void, List<
 	@Override
 	
 	protected List<Library> doInBackground(String... params) {
+		
+		List<Library> libraries = new ArrayList<Library>(0);
 		try {
-			Document doc = Jsoup.connect(Settings.libraryHourInfoPageUrl).get();
+			Document mHourPageDoc = Jsoup.connect(Settings.libraryHourInfoPageUrl).get();
+			Document availabilityDoc = Jsoup.connect(Settings.libraryComputerOverviewUrl).get();
 			
-			Element hoursTable = doc.getElementById("hourstable");
+			//Get the libraries and their opening hours.
+			libraries = getLibraryOpenHours(libraries, mHourPageDoc);
+			libraries = getLibraryComputerAvailabilities(libraries, availabilityDoc);
 			
-			Elements hoursTableRows = hoursTable.getElementsByTag("tr");
 			
-			//For each table row, except the first one
-			for (int i = 1; i < hoursTableRows.size(); i++)
-			{
-				Library library = new Library();
-				
-				Elements libraryName = hoursTableRows.get(i).select(".uqlnavigationlink");
-				
-				if (libraryName.size() > 0)
-				{
-					library.setName(libraryName.get(0).text());
-					Log.w("Library name", libraryName.get(0).text());
-				}
-				
-				
-				
-				//Now we look up today's cell. This is done by concatenating the current row to the first column.
-				
-				Element myCell = doc.getElementById("c"+String.valueOf(i) + "x1");
-				Log.w("Cell","c"+String.valueOf(i) + "x1");
-				if (myCell != null)
-				{
-					if (myCell.text().equals("CLOSED"))
-					{
-						library.setOpenToday(false);
-					} else {
-						library.setOpenToday(true);
-					}
-					
-					if (myCell.text().equals("open 24 hours"))
-					{
-						library.setOpenTimesToday(myCell.text());
-					} else if (!myCell.text().equals("CLOSED"))
-					{
-						library.setOpenTimesToday(myCell.text());
-					}
-					
-					Log.w("My cell times", myCell.text());
-					
-				}
-				
-			}
 			
 
 		} catch (IOException e) {
@@ -81,5 +45,85 @@ public class ASyncTaskScrapeUQLibraryInfo  extends AsyncTask<String, Void, List<
 		
 		return null;
 	}
+
+	private List<Library> getLibraryComputerAvailabilities(List<Library> libraries,
+			Document availabilityDoc) {
+		
+		Elements libraryRows = availabilityDoc.select(".left");
+		
+		for (Element lib : libraryRows)
+		{
+			
+		}
+		
+		
+		
+		return libraries;
+		
+	}
+
+	private List<Library> getLibraryOpenHours(List<Library> libraries,
+			Document mHourPageDoc) {
+		Element hoursTable = mHourPageDoc.getElementById("hourstable");
+		
+		Elements hoursTableRows = hoursTable.getElementsByTag("tr");
+		
+		int realRowIndex = 1;
+		boolean increaseRowIndex = false;
+		//For each table row, except the first one
+		for (int i = 1; i < hoursTableRows.size(); i++)
+		{
+			increaseRowIndex = false;
+			Library library = new Library();
+			
+			Elements libraryName = hoursTableRows.get(i).select(".uqlnavigationlink");
+			
+			if (libraryName.size() > 0)
+			{
+				library.setName(libraryName.get(0).text());
+				Log.w("Library name", libraryName.get(0).text());
+				increaseRowIndex = true;
+			} else {
+				
+			}
+			
+			
+			
+			//Now we look up today's cell. This is done by concatenating the current row to the first column.
+			
+			Element myCell = mHourPageDoc.getElementById("c"+String.valueOf(realRowIndex) + "x1");
+			//Log.w("Cell","c"+String.valueOf(realRowIndex) + "x1");
+			if (myCell != null)
+			{
+				if (myCell.text().equals("CLOSED"))
+				{
+					library.setOpenToday(false);
+				} else {
+					library.setOpenToday(true);
+				}
+				
+				if (myCell.text().equals("open 24 hours"))
+				{
+					library.setOpenTimesToday(myCell.text());
+				} else if (!myCell.text().equals("CLOSED"))
+				{
+					library.setOpenTimesToday(myCell.text());
+				}	
+				
+				Log.w("My cell times", myCell.text());
+				
+			}
+			
+			if (increaseRowIndex)
+			{
+				realRowIndex++;
+				libraries.add(library);
+			}
+			
+		}
+		return libraries;
+	}
+	
+	
 
 }
